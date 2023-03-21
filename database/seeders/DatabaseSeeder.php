6754ef44824e9fb12102a2b05110651a08c3365c
arthurariza <tucaariza@gmail.com>
2023-03-21 11:53:06 -0300

    Setup database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Company::factory()
+            ->count(10000)
+            ->has(User::factory()->count(10))
+            ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::find(10000);
+        $user->update([
+            'first_name' => 'Bill',
+            'last_name' => 'Gates',
+            'email' => 'bill.gates@microsoft.com',
+        ]);
+        $user->company->update([
+            'name' => 'Microsoft Corporation',
+        ]);
+
+        $user = User::find(20000);
+        $user->update([
+            'first_name' => 'Tim',
+            'last_name' => 'O\'Reilly',
+            'email' => 'tim@oreilly.com',
+        ]);
+        $user->company->update([
+            'name' => 'O\'Reilly Media Inc.',
+        ]);
     }
 }

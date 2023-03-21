da83a73d796674d1d17ee043d90238f14a28c407
arthurariza <tucaariza@gmail.com>
2023-03-21 13:41:26 -0300

    Users page

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,21 @@
+
+<script setup>
+import {Link} from "@inertiajs/vue3";
+defineProps({links: Object});
+</script>
+
+<template>
+    <div class="mt-4">
+    <Link
+        v-for="link in links"
+        :href="link.url"
+        preserve-scroll
+        v-html="link.label"
+        class="px-1 text-blue-500 text-sm"
+        :class="{'font-bold text-2xl' : link.active }"
+    />
+    </div>
+</template>
+
+<style scoped>
+</style>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,10 +1,24 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import {Head} from "@inertiajs/vue3";
+import {Head, router} from "@inertiajs/vue3";
+import {ref, watch} from "vue";
+import Pagination from "@/Components/Pagination.vue";
 
-    defineProps({
-        users: Object
-    });
+const props = defineProps({
+    filters: Object,
+    users: Object,
+});
+
+let search = ref(props.filters?.search);
+
+watch(search, (value) => {
+    router.get('/users',
+        {search: value},
+        {
+            preserveState: true,
+            replace: true
+        })
+});
 
 </script>
 
@@ -16,7 +30,62 @@ import {Head} from "@inertiajs/vue3";
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Users</h2>
         </template>
 
+        <main class="max-w-6xl mx-auto sm:px-6 lg:px-8 py-12">
+            <form class="max-w-lg">
+                <label for="search" class="sr-only">Search</label>
+                <div class="relative rounded-md shadow-sm">
+                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <svg class="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                    <input id="search" name="search" v-model="search" class="form-input block w-full pl-10 sm:text-sm sm:leading-5" placeholder="Search..." autofocus />
+                </div>
+            </form>
+            <div>
 
+            <Pagination :links="users.links"/>
+            </div>
+
+            <div class="mt-4 flex flex-col">
+                <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                    <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg">
+                        <table class="min-w-full">
+                            <thead>
+                            <tr>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Email
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Company
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr class="bg-white" v-for="user in users.data" :key="user.id">
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
+                                    {{ user.first_name }} {{ user.last_name }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                    {{ user.email }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                    {{ user.company.name }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline">Edit</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </main>
     </AuthenticatedLayout>
 </template>
 

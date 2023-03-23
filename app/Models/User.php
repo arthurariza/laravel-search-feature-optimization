7c872b7f0436b6df52d9427a5e0122f8e56ef340
arthurariza <tucaariza@gmail.com>
2023-03-22 22:29:13 -0300

    Use Unions to Run Queries Independently

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,13 +48,22 @@ class User extends Authenticatable
     {
         collect(str_getcsv($terms, ' ', '"'))->filter()->each(function ($term) use ($query) {
             $term = $term . '%';
-            $query->where(function ($query) use ($term) {
-               $query->where('first_name', 'like', $term)
-                ->orWhere('last_name', 'like', $term)
-                ->orWhereIn('company_id', Company::query()
-                    ->where('name', 'like', $term)
-                    ->pluck('id')
-                );
+
+            $query->whereIn('id', function ($query) use ($term) {
+                $query->select('id')
+                    ->from(function ($query) use ($term) {
+                        $query->select('id')
+                            ->from('users')
+                            ->where('first_name', 'like', $term)
+                            ->orWhere('last_name', 'like', $term)
+                            ->union(
+                                $query->newQuery()
+                                    ->select('users.id')
+                                    ->from('users')
+                                    ->join('companies', 'users.company_id', '=', 'companies.id')
+                                    ->where('companies.name', 'like', $term)
+                            );
+                    }, 'matches');
             });
         });
     }
